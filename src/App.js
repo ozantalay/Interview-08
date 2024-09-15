4a585f5e4dda6ba1f480fdc8e13c92726e4bb057
ozantalay <ozntalay@hotmail.com>
2024-09-16 02:18:39 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -23,14 +23,42 @@ const ValidatedForm = () => {
 
   const onSubmit = (e) => {
     // KODUNUZ BURAYA GELECEK
+    e.preventDefault();
+    if(username.length>20 || password.length>20){
+      alert('kullanıcı adı ve şifre 20 karakterden fazla olamaz.')
+      return
+        }
+        if(username.length<6 || password.length<6){
+          alert('kullanıcı adı ve şifre 6 karakterden az olmamalı')
+          return
+        }
+          const availableAccount = accounts.find(account => account.username === username && account.password === password);
+         
+        
+        if(availableAccount){
+          alert(`hoş geldin ${username}!`)
+          setUsername('')
+          setPassword('')
+        }
+        else{
+          const newAccount={username,password}
+          setAccounts([...accounts,newAccount])
+          alert(`yeni bir hesap oluşturdun ${username}!`)
+          setUsername('')
+          setPassword('')
+        
+        }
+       
+    
   };
+
 
   return (
     <form
       style={{
         display: "flex",
         flexDirection: "column",
-        border: "solid",
+        border: "1px solid",
         padding: 10
       }}
       onSubmit={onSubmit}
@@ -39,14 +67,14 @@ const ValidatedForm = () => {
       <input
         value={username}
         type="text"
-        onChange={/* KODUNUZ BURAYA GELECEK */ () => {}}
-        style={{ marginBottom: 5 }}
+        onChange={/* KODUNUZ BURAYA GELECEK */ (e) => setUsername(e.target.value)}
+        style={{ marginBottom: 5 ,border:'1px solid black'}}
       />
       <input
         value={password}
         type="text"
-        onChange={/* KODUNUZ BURAYA GELECEK */ () => {}}
-        style={{ marginBottom: 10 }}
+        onChange={/* KODUNUZ BURAYA GELECEK */ (e) => setPassword(e.target.value)}
+        style={{ marginBottom: 10 ,border:'1px solid black'}}
       />
       <button style={{ alignSelf: "center" }} onClick={onSubmit}>
         Submit
